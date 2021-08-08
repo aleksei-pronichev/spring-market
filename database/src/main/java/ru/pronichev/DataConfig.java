4e19fc065b7372df474ff9833798912857d3b810
aleksei pronichev <aleksei.pronichev@outlook.com>
2021-08-08 18:08:09 +0300

    app configurations

---
+++ database/src/main/java/ru/pronichev/DataConfig.java
@@ -0,0 +1,10 @@
+package ru.pronichev;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataConfig {
+    public static void main(String[] args) {
+        // configuration class for data library
+    }
+}
