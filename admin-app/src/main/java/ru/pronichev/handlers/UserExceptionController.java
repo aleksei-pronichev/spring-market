ffe04beb21e1be3d49b4979b26f8960d5f686760
aleksei pronichev <aleksei.pronichev@outlook.com>
2021-08-22 13:08:00 +0300

    add errors with delete operation

--- admin-app/src/main/java/ru/pronichev/handlers/UserExceptionController.java
+++ admin-app/src/main/java/ru/pronichev/handlers/UserExceptionController.java
@@ -1,5 +1,6 @@
 package ru.pronichev.handlers;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,10 +9,20 @@ import ru.pronichev.exceptions.NotFoundException;
 
 @ControllerAdvice
 public class UserExceptionController {
+
     @ExceptionHandler(value = NotFoundException.class)
     public ModelAndView notFound(NotFoundException exception) {
         ModelAndView modelAndView = new ModelAndView("not_found");
         modelAndView.addObject("message", exception.getMessage());
+        modelAndView.setStatus(HttpStatus.NOT_FOUND);
+        return modelAndView;
+    }
+
+    @ExceptionHandler(value = SQLIntegrityConstraintViolationException.class)
+    public ModelAndView sqlViolation(SQLIntegrityConstraintViolationException exception) {
+        ModelAndView modelAndView = new ModelAndView("operation_error");
+        modelAndView.addObject("message", "This object is used in others ones, delete them first");
+        modelAndView.addObject("description", exception.getMessage());
         modelAndView.setStatus(HttpStatus.NOT_FOUND);
         return modelAndView;
     }
